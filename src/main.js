954523b717bbad06acd12ab6d3a71ca54c0279ad
Liyiheng <liyiheng26@gmail.com>
2023-04-06 23:58:51 +0800

    回溯

--- src/main.js
+++ src/main.js
@@ -42,20 +42,20 @@
 // });
 
 //响应josn格式的数据：通过转换json格式的数据是客户端能懂需要干什么
-const http = require("http");
-const server = http.createServer((request, response) => {
-  const data = {
-    id: 1,
-    title: "关山月",
-    content: "日照香炉生紫烟",
-  };
-  const josnData = JSON.stringify(data); //转换json格式
-  response.writeHead(200, {
-    "content-type": "application/json; charset=utf-8",
-  });
-  response.write(josnData);
-  response.end();
-});
-server.listen(3000, () => {
-  console.log("🚀服务已启动！");
-});
+// const http = require("http");
+// const server = http.createServer((request, response) => {
+//   const data = {
+//     id: 1,
+//     title: "关山月",
+//     content: "日照香炉生紫烟",
+//   };
+//   const josnData = JSON.stringify(data); //转换json格式
+//   response.writeHead(200, {
+//     "content-type": "application/json; charset=utf-8",
+//   });
+//   response.write(josnData);
+//   response.end();
+// });
+// server.listen(3000, () => {
+//   console.log("🚀服务已启动！");
+// });
