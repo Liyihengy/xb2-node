daf199353ae821c7b420eb7c60eaf6423eda3818
Liyiheng <liyiheng26@gmail.com>
2023-05-23 11:25:24 +0800

    改进创建HTTP服务器的方法

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 //创建web服务器
 //导入
-import app from './app';
+import httpServer from './app/app.server';
 //导入配置好的环境变量
 import { APP_PORT } from './app/app.config';
 import { connection } from './app/database/mysql';
 
 //创建一个端口3000,然后在控制台输出文字
-app.listen(APP_PORT, () => {
+httpServer.listen(APP_PORT, () => {
   console.log('🚀 服务已启动！');
 });
 
