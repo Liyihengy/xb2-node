cd9aa8775a716bbdbdec3a0b9656486c8d096d2a
Liyiheng <liyiheng26@gmail.com>
2023-05-23 15:06:16 +0800

    测试实时服务器与客户端之间的链接

--- src/app/app.server.ts
+++ src/app/app.server.ts
@@ -1,10 +1,28 @@
 import http from 'http';
+import { Server } from 'socket.io';
 import app from './index';
+import { ALLOW_ORIGIN } from './app.config';
 
 /**
  * HTTP 服务器
  */
 const httpServer = http.createServer(app);
+
+/**
+ * IO实时服务器
+ */
+const socketIoServer = new Server(httpServer, {
+  cors: {
+    origin: ALLOW_ORIGIN,
+    allowedHeaders: ['X-Total-Count'],
+  },
+});
+
+socketIoServer.on('connect', socket => {
+  socket.on('greet', data => {
+    console.log(data);
+  });
+});
 
 /**
  * 默认导出
